5754adcab08adcb581e99c7179cbec164dcc6834
FerasHamdan <ferasforfuture@gmail.com>
2023-12-01 21:12:44 +0200

    edit 1

--- ss/models.py
+++ ss/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Person(models.Model):
     name=models.CharField(max_length=15)
+    age=models.IntegerField(default=0)
 
 
     def __str__(self):
